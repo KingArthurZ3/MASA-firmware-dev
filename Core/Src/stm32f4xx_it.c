23fc6f5399994cb2532bd9033167ef5f34ad9bc5
KingArthurZ3 <arthurzh@umich.edu>
2020-06-21 12:52:49 -0700

    configured project to use git submodule for adc library

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -207,7 +207,7 @@ void TIM2_IRQHandler(void)
   /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
-  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+  //HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
   /* USER CODE END TIM2_IRQn 1 */
 }
 
